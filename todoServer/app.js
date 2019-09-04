98e7bcd2feb6aa1c341a3a088092dc8628c11979
chengeheng <465819676@qq.com>
2019-09-04 14:52:10 +0800

    增加内容模糊搜索

--- todoServer/app.js
+++ todoServer/app.js
@@ -8,24 +8,26 @@ const mongoose = require("mongoose");
 
 var app = express();
 
-mongoose.connect("mongodb://localhost:27017/todo", () =>
-  console.log("Connection has been made").catch(err => {
-    console.error("App starting error:", err.stack);
-    process.exit(1);
-  })
-);
+mongoose.connect("mongodb://localhost:27017/todo", err => {
+    if (err) {
+        console.error("App starting error:", err.stack);
+        process.exit(1);
+    } else {
+        console.log("Connection has been made");
+    }
+});
 
 const db = mongoose.connection;
 
 db.on("connected", () => {
-  console.log("Mongoose connection open ");
+    console.log("Mongoose connection open ");
 });
 
 db.on("error", err => console.log("Mongoose connection error" + err));
 db.on("disconnected", _ => console.log("Mongoose connection disconnected"));
 
 app.use(logger("dev"));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // 使用中间件
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use("/todo", todoRouter);
--- todoServer/routes/todo.js
+++ todoServer/routes/todo.js
@@ -1,72 +1,77 @@
 const express = require("express");
 const todo = require("./../models/todo");
 const router = express.Router();
+const url = require("url");
+const qs = require("querystring");
 
 router.post("/add", (req, res) => {
-  let result = Object.assign(
-    {},
-    {
-      ...req.query,
-      timeStamp: new Date().getTime(),
-      completed: req.query.completed == "true"
-    }
-  );
-  todo.create(result, (err, doc) => {
-    if (err) console.log("error", err);
-    console.log(doc);
-  });
-  res.json(result);
+    let result = Object.assign(
+        {},
+        {
+            ...req.query,
+            timeStamp: new Date().getTime(),
+            completed: req.query.completed == "true"
+        }
+    );
+    todo.create(result, (err, doc) => {
+        if (err) console.log("error", err);
+        console.log(doc);
+    });
+    res.json(result);
 });
 
 router.post("/edit", (req, res) => {
-  let result = Object.assign(
-    {},
-    {
-      ...req.query
-    }
-  );
-  todo.updateOne(
-    { id: req.param("id") },
-    { $set: { remark: result.remark, text: result.text } },
-    (err, raw) => {
-      if (err) console.log("err", err);
-    }
-  );
-  res.json(result);
+    let result = Object.assign(
+        {},
+        {
+            ...req.query
+        }
+    );
+    todo.updateOne(
+        { id: req.param("id") },
+        { $set: { remark: result.remark, text: result.text } },
+        (err, raw) => {
+            if (err) console.log("err", err);
+        }
+    );
+    res.json(result);
 });
 
 router.post("/complete", (req, res) => {
-  let result = Object.assign(
-    {},
-    {
-      ...req.query,
-      completed: true
-    }
-  );
-  todo.updateOne(
-    { id: req.param("id") },
-    { $set: { completed: true } },
-    (err, raw) => {
-      if (err) console.log("err", err);
-    }
-  );
-  res.json(result);
+    let result = Object.assign(
+        {},
+        {
+            ...req.query,
+            completed: true
+        }
+    );
+    todo.updateOne(
+        { id: req.param("id") },
+        { $set: { completed: true } },
+        (err, raw) => {
+            if (err) console.log("err", err);
+        }
+    );
+    res.json(result);
 });
 
 router.get("/list", (req, res) => {
-  todo.find((err, doc) => {
-    if (err) console.log("list error", err);
-    res.status(200);
-    res.json(doc);
-  });
+    const { text = "", date = "" } = qs.parse(url.parse(req.url).query);
+    // console.log(date);
+    // todo.find({ text: { $regex: text }, timeStamp: { $regex: date } }).exec(
+    todo.find({ text: { $regex: text } }).exec((err, doc) => {
+        if (err) console.log("list error", err);
+        res.status(200);
+        res.json(doc);
+    });
 });
 
 router.delete("/del", (req, res) => {
-  todo.findOneAndRemove({ id: req.param("id") }, err => {
-    if (err) console.log("remove", err);
-  });
-  res.status(200);
-  res.json(req.query);
+    todo.findOneAndRemove({ id: req.param("id") }, err => {
+        if (err) console.log("remove", err);
+    });
+    res.status(200);
+    res.json(req.query);
 });
 
 module.exports = router;
